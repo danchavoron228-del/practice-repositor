08a2c8779cdc55229cdc6a71ba525c056067edd0
воронин данила <DanchaVoron@yandex.ru>
2026-05-10 20:11:10 +0300

    Comment PluginLoader to fix compilation

--- src/main/java/ru/mtuci/coursemanagement/controller/IndexController.java
+++ src/main/java/ru/mtuci/coursemanagement/controller/IndexController.java
@@ -4,16 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.mtuci.coursemanagement.service.PluginLoader;
+// import ru.mtuci.coursemanagement.service.PluginLoader; // временно закомментировано
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
-    private final PluginLoader loader;
+    // private final PluginLoader loader; // временно закомментировано
 
     @GetMapping("/")
     public String index(Model model) {
-        loader.tryLoad();
+        // loader.tryLoad(); // временно закомментировано
         return "index";
     }
 }
