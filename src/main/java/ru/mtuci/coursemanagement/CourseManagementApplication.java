f5415e453b1ad18c1417cd8c049a96858bf073f2
воронин данила <DanchaVoron@yandex.ru>
2026-05-11 12:18:07 +0300

    Fix: recreate main class without BOM

--- src/main/java/ru/mtuci/coursemanagement/CourseManagementApplication.java
+++ src/main/java/ru/mtuci/coursemanagement/CourseManagementApplication.java
@@ -1,4 +1,4 @@
-﻿package ru.mtuci.coursemanagement;
+package ru.mtuci.coursemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
